e45bb5e8bab243c2020d95c678a49aa632fb8e64
sunpochin@gmail.com <sunpochin@github.com>
2017-08-27 03:57:08 +0000

    Try 1 e-5 decay, RMSprop.

--- params.py
+++ params.py
@@ -4,9 +4,9 @@ from model.u_net import get_convnet_simple, get_unet_1918_1280
 # use larger downsample for faster modeling, but not accurate.
 downsample = 1
 #input_size = 128
-#input_size = 1024
+input_size = 1024
 # 0 meaning NO resize.
-input_size = 0
+#input_size = 0
 
 max_epochs = 50
 #batch_size = 16
@@ -19,7 +19,7 @@ threshold = 0.5
 
 #model = get_unet_128()
 #model = get_convnet_simple()
-#model = get_unet_1024()
-model = get_unet_1918_1280()
+model = get_unet_1024()
+#model = get_unet_1918_1280()
 
 
